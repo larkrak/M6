6982d70f58559c3aaccf9b1fe18359d792feb916
Larkrak <larkrak010@gmail.com>
2020-11-03 15:26:10 +0100

    summarybuttons

--- Practica2/js/control/popUpWindow.js
+++ Practica2/js/control/popUpWindow.js
@@ -2,45 +2,42 @@
 function loadData(){
 
     frame = window.opener.document;
-
     table = frame.getElementById("contentTable");
     tableSummary = document.getElementById("tableSummary");
+    document.getElementById("data").innerText = frame.getElementById("resultSelectP").innerText;
+    document.getElementById("spanTotal").innerText = (table.rows.length) - 1
+    
+    var d = new Date();
+    var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    document.getElementById("time").innerHTML = days[d.getDay()] + ", " +d.getDate() + " of " + months[d.getMonth()] + " of " + d.getFullYear();
 
     console.log(table)
     arrVals = []
 
-    for (let index = 1; index < table.rows.length; index++) {
+    for (let index = 3; index < (table.rows.length)+2; index++) {
         
+        console.log(table.rows.length)
+
         newRow = document.getElementById("tableSummary").insertRow()
+        newRow.classList.add("bordered")
         newRow.insertCell(0)
         newRow.insertCell(1)
         newRow.insertCell(2)
 
-        for (let c = 0; c < table.rows[index].cells.length; c++) {
+        for (let c = 0; c < 3; c++) {
 
-            contentForEachTD = table.rows[index].cells[c].childNodes[0];
+            contentForEachTD = table.rows[index-2].cells[c].childNodes[0];
             if(contentForEachTD.type == 'text'){
-                arrVals.push(contentForEachTD.value)
+                tableSummary.rows[index].cells[c].innerText = contentForEachTD.value;
             }else{
-                arrVals.push(contentForEachTD.checked)
-            }
-            
+                tableSummary.rows[index].cells[c].innerText = contentForEachTD.checked;
+            }   
         }
     }
 
     console.log(tableSummary)
 
-    for (let index = 1; index < tableSummary.rows.length; index++) {
-        
-        for (let c = 0; c < 3; c++) {
-            
-            tableSummary.rows[index].cells[c].innerText = arrVals[c];
-
-            console.log(arrVals[c])
-            
-        }
-        
-    }
 
     console.log(arrVals)
 
