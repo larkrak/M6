653d3683918aca6394fe00ebafdf88d6c6130379
Larkrak <larkrak010@gmail.com>
2020-11-03 14:06:14 +0100

    summary

--- Practica2/js/control/popUpWindow.js
+++ Practica2/js/control/popUpWindow.js
@@ -1,50 +1,47 @@
-function loadsData(){
-    //This object contains all the information about the iframe object (opener)
-    var frame = window.opener.document;
-    
-    var propertyType = frame.getElementById("propertyType").value;
-    var numberProperties = frame.getElementById("numberProperties").value;
-
-    console.log(propertyType);
-    console.log(numberProperties);
-    
-    //This parent object referencer index.html
-    var parent = window.opener.parent.document;   
-    var myName = parent.getElementById("myName").value;
-
-    document.getElementById("name").innerText = myName;
-    document.getElementById("propertyType").innerText = propertyType;
-    
-    var strAux="";
-    for (let index = 0; index < numberProperties; index++) {
-        strAux += "<TR><TD>"+ propertyType +"</TD></TR>";
-    }
-
-    document.getElementById("tableClients").innerHTML += strAux;
-}
 
 function loadData(){
 
     frame = window.opener.document;
-    parent = window.opener.parent.document;
 
-    //specieType = frame.getElementById("propertyType").value;
+    table = frame.getElementById("contentTable");
+    tableSummary = document.getElementById("tableSummary");
 
-    table = frame.getElementById("contentTable")
+    console.log(table)
+    arrVals = []
 
-    lenR = table.rows.length
+    for (let index = 1; index < table.rows.length; index++) {
+        
+        newRow = document.getElementById("tableSummary").insertRow()
+        newRow.insertCell(0)
+        newRow.insertCell(1)
+        newRow.insertCell(2)
 
-    
-    for (let index = 1; index < lenR; index++) {
-        for (var c = 0, m = table.rows[index].cells.length; c < m; c++) {
-            input = table.rows[index].cells[c];
+        for (let c = 0; c < table.rows[index].cells.length; c++) {
 
-            console.log(input.children)
+            contentForEachTD = table.rows[index].cells[c].childNodes[0];
+            if(contentForEachTD.type == 'text'){
+                arrVals.push(contentForEachTD.value)
+            }else{
+                arrVals.push(contentForEachTD.checked)
+            }
+            
+        }
+    }
+
+    console.log(tableSummary)
+
+    for (let index = 1; index < tableSummary.rows.length; index++) {
+        
+        for (let c = 0; c < 3; c++) {
+            
+            tableSummary.rows[index].cells[c].innerText = arrVals[c];
+
+            console.log(arrVals[c])
+            
         }
         
     }
 
-
-    
+    console.log(arrVals)
 
 }
